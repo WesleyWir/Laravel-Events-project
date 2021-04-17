6d1e19dd0a1edae9571fe289279a0aa2ede07b1d
Wesley Wirmond <wesleywirmond@gmail.com>
2021-04-17 02:36:24 -0300

    DATABASE RELATIONS USER->EVENT

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -48,6 +48,9 @@ class EventController extends Controller
             $event->image = $imageName;
         }
         
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento Criado com sucesso');
